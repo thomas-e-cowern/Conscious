7d06a14ecfded2854efcf9651c67825456e44410
Hanadi <alothman.hanadi@gmail.com>
2019-01-17 15:28:17 -0700

    Organized all models and modelcontrollers

---
+++ Conscious/ViewControllers/QuizViewController.swift
@@ -0,0 +1,24 @@
+//
+//  QuizViewController.swift
+//  Conscious
+//
+//  Created by Hanadi AlOthman on 17/01/2019.
+//  Copyright © 2019 Thomas Cowern New. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    // MARK: - Outlets
+    
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+    
+    
+}
