93d519267127aa08ee7941db36eee36e5dbd374e
Thomas E Cowern <thomas.e.cowern@gmail.com>
2019-01-17 13:47:42 -0700

    adds tab bar view

--- Conscious/OnboardingPageViewController.swift
+++ Conscious/OnboardingPageViewController.swift
@@ -33,15 +33,19 @@ class OnboardingPageViewController: UIPageViewController {
         return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(name)ViewController")
     }
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if segue.description == "toQuiz" {
+            let destinationVC = segue.destination as UIViewController
+        
+        }
     }
-    */
+ 
 
 }
 
