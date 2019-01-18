ce612bcb4032bd9290c4ba89e08b37fde42b5ad3
Hanadi <alothman.hanadi@gmail.com>
2019-01-17 17:11:42 -0700

    Adds Quiz View Controller and Question Model and organizes projects folders and files

--- Conscious/ViewControllers/QuizViewController.swift
+++ Conscious/ViewControllers/QuizViewController.swift
@@ -11,14 +11,43 @@ import UIKit
 class QuizViewController: UIViewController {
 
     // MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var quizProgressView: UIProgressView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var skipButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var previousButton: UIButton!
     
+    // MARK: - Properties
+    var categories = ["Food", "House", "Travel"]
+    var food: Food?
+    var house: House?
+    var travel: Travel?
+    
+    var question: Question?
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
+    }
+    
+    // MARK: - Setup
+    func updateViews() {
     }
     
     // MARK: - Actions
+    @IBAction func skipButtonTapped(_ sender: Any) {
+    }
     
+    
+    @IBAction func nextButtonTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func previousButtonTapped(_ sender: Any) {
+    }
     
 }
