c9a9b63a84536481729960eb058ff37cfd7e8fe6
Hanadi <alothman.hanadi@gmail.com>
2019-01-21 16:39:46 -0700

    Bugs fix

--- Conscious/ViewControllers/SettingsViewController.swift
+++ Conscious/ViewControllers/SettingsViewController.swift
@@ -9,28 +9,15 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
     }
     
+    // MARK: - Actions
     @IBAction func doneButtonTapped(_ sender: Any) {
-        
         navigationController?.popViewController(animated: true)
         dismiss(animated: true, completion: nil)
-        
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
