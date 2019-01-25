0820fa9065f971062b02e7b89a6211f32fd91076
Hanadi <alothman.hanadi@gmail.com>
2019-01-25 15:10:07 -0700

    adds terms of service page & fixes navigation & tab bar landing page

---
+++ Conscious/ViewControllers/TabBarControllor.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarControllor.swift
+//  Conscious
+//
+//  Created by Hanadi AlOthman on 25/01/2019.
+//  Copyright © 2019 Thomas Cowern New. All rights reserved.
+//
+
+import UIKit
+
+class TabBarControllor: UITabBarController {
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 1
+        navigationItem.setHidesBackButton(true, animated: false)
+    }
+}
