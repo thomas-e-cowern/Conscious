4115a098f2e76acdf5148dd30b29f149aa0b7930
Thomas E Cowern <thomas.e.cowern@gmail.com>
2019-01-30 16:59:58 -0700

    completes actionList

--- Conscious/ModelControllers/LocalStorageController.swift
+++ Conscious/ModelControllers/LocalStorageController.swift
@@ -17,94 +17,89 @@ class LocalStorageController {
     var savedData: [SavedData] = []
     
     var savedActions: [ActionPlanDetail] = []
+//    
+//    func addNewResults(overallScore: Double, foodScore: Double, houseScore: Double, travelScore: Double) {
+//        let newResults = SavedData(overallScore: overallScore, foodScore: foodScore, houseScore: houseScore, travelScore: travelScore)
+//        savedData.append(newResults)
+//        self.saveToPersistentStoreData(path: "data")
+//    }
+//    
+//    func addNewActions(action: ActionPlanDetail) {
+//        print("🧸🧸🧸🧸🧸\(action)🧸🧸🧸🧸🧸")
+//        let newAction = action
+//        savedActions.append(newAction)
+//        print("🎈🎈🎈🎈🎈\(savedActions)🎈🎈🎈🎈🎈")
+//        self.saveToPersistentStoreData(path: "action")
+//    }
+//    
+//    func saveActions(actions: [ActionPlanDetail]) {
+//        savedActions = actions
+//        print("🎈🎈🎈🎈🎈\(savedActions[0].completed)🎈🎈🎈🎈🎈")
+//        self.saveToPersistentStoreData(path: "action")
+//    }
+//    
+//    func deleteAction(index: Int?) {
+//        guard let index = index else { return }
+//        print("ID: \(index)")
+//        savedActions.remove(at: index)
+//        print("deleted")
+//        self.saveToPersistentStoreData(path: "action")
+//    }
     
-    func addNewResults(overallScore: Double, foodScore: Double, houseScore: Double, travelScore: Double) {
-        let newResults = SavedData(overallScore: overallScore, foodScore: foodScore, houseScore: houseScore, travelScore: travelScore)
-        savedData.append(newResults)
-        self.saveToPersistentStoreData(path: "data")
-    }
-    
-    func addNewActions(action: ActionPlanDetail) {
-        print("🧸🧸🧸🧸🧸\(action)🧸🧸🧸🧸🧸")
-        let newAction = action
-        savedActions.append(newAction)
-        print("🎈🎈🎈🎈🎈\(savedActions)🎈🎈🎈🎈🎈")
-        self.saveToPersistentStoreData(path: "action")
-    }
-    
-    func saveActions(actions: [ActionPlanDetail]) {
-        savedActions = actions
-        print("🎈🎈🎈🎈🎈\(savedActions[0].completed)🎈🎈🎈🎈🎈")
-        self.saveToPersistentStoreData(path: "action")
-    }
-    
-    func updateActionStatus(action: ActionPlanDetail, completed: Bool) {
-//        var savedActions = loadFromPersistenceStoreData(path: "actions")
-//        print("\(savedActions.count)")
-//        for i in 0..<savedActions.count {
-//            if savedActions[i].action == action.action {
-//                print("🧢🧢🧢🧢🧢🧢🧢🧢Matching🧢🧢🧢🧢🧢🧢🧢🧢🧢")
-//                savedActions[i].completed = completed
+//    // Saving data
+//    func fileURL(path: String) -> URL {
+//        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+//        let documentsDirectory = paths[0]
+//        let fileName = "\(path).json"
+//        let fullURL = documentsDirectory.appendingPathComponent(fileName)
+//        
+//        print(fullURL)
+//        
+//        return(fullURL)
+//    }
+//    
+//    func saveToPersistentStoreData(path: String) {
+//        let encoder = JSONEncoder()
+//        if path == "data" {
+//            do {
+//                let data = try encoder.encode(self.savedData)
+//                try data.write(to: fileURL(path: path))
+//            } catch {
+//                print("☎️☎️☎️☎️☎️Error: \(#function): \(error) : \(error.localizedDescription)☎️☎️☎️☎️☎️")
+//            }
+//        } else {
+//            do {
+//                let data = try encoder.encode(self.savedActions)
+//                try data.write(to: fileURL(path: path))
+//            } catch {
+//                print("☎️☎️☎️☎️☎️Error: \(#function): \(error) : \(error.localizedDescription)☎️☎️☎️☎️☎️")
 //            }
 //        }
-//        self.saveToPersistentStoreData(path: "actions")
-    }
-    
-    // Saving data
-    func fileURL(path: String) -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        let documentsDirectory = paths[0]
-        let fileName = "\(path).json"
-        let fullURL = documentsDirectory.appendingPathComponent(fileName)
-        
-        print(fullURL)
-        
-        return(fullURL)
-    }
-    
-    func saveToPersistentStoreData(path: String) {
-        let encoder = JSONEncoder()
-        if path == "data" {
-            do {
-                let data = try encoder.encode(self.savedData)
-                try data.write(to: fileURL(path: path))
-            } catch {
-                print("☎️☎️☎️☎️☎️Error: \(#function): \(error) : \(error.localizedDescription)☎️☎️☎️☎️☎️")
-            }
-        } else {
-            do {
-                let data = try encoder.encode(self.savedActions)
-                try data.write(to: fileURL(path: path))
-            } catch {
-                print("☎️☎️☎️☎️☎️Error: \(#function): \(error) : \(error.localizedDescription)☎️☎️☎️☎️☎️")
-            }
-        }
+////        do {
+////            let data = try encoder.encode(self.savedData)
+////            try data.write(to: fileURL(path: path))
+////        } catch {
+////            print("☎️☎️☎️☎️☎️Error: \(#function): \(error) : \(error.localizedDescription)☎️☎️☎️☎️☎️")
+////        }
+//    }
+//    
+//    func loadFromPersistenceStore<T: Codable>(path: String) -> [T] {
 //        do {
-//            let data = try encoder.encode(self.savedData)
-//            try data.write(to: fileURL(path: path))
-//        } catch {
-//            print("☎️☎️☎️☎️☎️Error: \(#function): \(error) : \(error.localizedDescription)☎️☎️☎️☎️☎️")
+//            let data = try Data(contentsOf: fileURL(path: path))
+//            
+//            let decoder = JSONDecoder()
+//            let returnData = try decoder.decode([T].self, from: data)
+//            
+//            if T.self == SavedData.self{
+//                savedData = returnData as! [SavedData]
+//            }else{
+//                savedActions = returnData as! [ActionPlanDetail]
+//            }
+//            return returnData
+//        } catch  {
+//            print("🔥🔥🔥🔥🔥Error: \(#function): \(error) : \(error.localizedDescription)🔥🔥🔥🔥🔥")
 //        }
-    }
-    
-    func loadFromPersistenceStore<T: Codable>(path: String) -> [T] {
-        do {
-            let data = try Data(contentsOf: fileURL(path: path))
-            
-            let decoder = JSONDecoder()
-            let returnData = try decoder.decode([T].self, from: data)
-            
-            if T.self == SavedData.self{
-                savedData = returnData as! [SavedData]
-            }else{
-                savedActions = returnData as! [ActionPlanDetail]
-                print("🧿🧿🧿🧿🧿returnData: \(savedActions[0].completed)🧿🧿🧿🧿🧿")
-            }
-            return returnData
-        } catch  {
-            print("🔥🔥🔥🔥🔥Error: \(#function): \(error) : \(error.localizedDescription)🔥🔥🔥🔥🔥")
-        }
-        
-        return []
-    }
+//        
+//        return []
+//    }
 }
--- Conscious/ViewControllers/MyActionsViewController.swift
+++ Conscious/ViewControllers/MyActionsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class MyActionsViewController: UIViewController{
-
+    
     @IBOutlet weak var myActionsTableview: UITableView!
     @IBOutlet weak var actionsCompleteLabel: UILabel!
     @IBOutlet weak var actionsPledged: UILabel!
@@ -20,37 +20,41 @@ class MyActionsViewController: UIViewController{
     var totalCarbonSavings: Double = 0
     var actionsCompletedCount: Int = 0
     var savedActions: [ActionPlanDetail] = []
-    var intialCount: Int = 0
     
-    fileprivate func setUpCarbonReduction(int: Int) {
-        for i in 0..<int - 1 {
-            totalCarbonSavings += actionsCompleted[i].carbonReduction ?? 0
-            print("Tcc: \(totalCarbonSavings)")
+    fileprivate func updateCarbonSaved() {
+        for i in 0..<actionsCompletedCount {
+            if actionsCompletedCount == 0 {
+                print("i is zero")
+            } else {
+                totalCarbonSavings += actionsCompleted[i].carbonReduction ?? 0
+            }
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        intialCount = 0
-        let loadedActions: [ActionPlanDetail] = LocalStorageController.shared.loadFromPersistenceStore(path: "action")
+        let loadedActions: [ActionPlanDetail] = ActionPlanController.shared.loadFromPersistenceStore(path: "action")
         self.savedActions = loadedActions
-        print("👠👠👠👠👠👠👠👠\(savedActions[0].completed)👠👠👠👠👠👠👠👠👠👠👠")
+        //        print("👠👠👠👠👠👠👠👠\(savedActions[0].completed)👠👠👠👠👠👠👠👠👠👠👠")
         myActions = savedActions
         print("🥼🥼🥼🥼🥼🥼🥼🥼🥼🥼\(myActions)🥼🥼🥼🥼🥼🥼🥼🥼🥼🥼")
         myActionsTableview.dataSource = self
-        actionsCompletedCount = myActions.filter { $0.completed == true }.count
+        actionsPledged.text = " of \(myActions.count)"
+        actionsCompleted = myActions.filter { $0.completed == true }
+        actionsCompletedCount = actionsCompleted.count
         print("Acc: \(actionsCompletedCount)")
-        setUpCarbonReduction(int: actionsCompletedCount)
+        updateCarbonSaved()
+        print("TCS: \(totalCarbonSavings)")
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        myActions = savedActions
+        myActions = ActionPlanController.shared.loadFromPersistenceStore(path: "action")
         updateViews()
     }
     
     func updateViews() {
-        myActions = savedActions
+        myActions = ActionPlanController.shared.loadFromPersistenceStore(path: "action")
         actionsCompleteLabel.text = "\(actionsCompletedCount)"
         actionsPledged.text = " of \(myActions.count)"
         let carbonSavingString = String(format: "%.2f", totalCarbonSavings/52)
@@ -60,7 +64,7 @@ class MyActionsViewController: UIViewController{
 }
 
 extension MyActionsViewController: UITableViewDataSource {
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return myActions.count
     }
@@ -73,14 +77,7 @@ extension MyActionsViewController: UITableViewDataSource {
         let testTitle = myActions[indexPath.row].action
         let attributedString = NSMutableAttributedString(string: testTitle)
         cell.actionViewCellLabel.text = myActions[indexPath.row].action
-        switch myActions[indexPath.row].icon {
-        case "Food":
-            cell.actionTableViewImage.image = UIImage(named: "FoodUnchecked")
-        case "house":
-            cell.actionTableViewImage.image = UIImage(named: "HomeUnchecked")
-        default:
-            cell.actionTableViewImage.image = UIImage(named: "TravelUnchecked")
-        }
+        
         if myActions[indexPath.row].completed == true {
             switch myActions[indexPath.row].icon {
             case "Food":
@@ -93,20 +90,49 @@ extension MyActionsViewController: UITableViewDataSource {
             attributedString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: attributedString.length))
             cell.actionViewCellLabel.attributedText = attributedString
         } else {
+            switch myActions[indexPath.row].icon {
+            case "Food":
+                cell.actionTableViewImage.image = UIImage(named: "FoodUnchecked")
+            case "house":
+                cell.actionTableViewImage.image = UIImage(named: "HomeUnchecked")
+            default:
+                cell.actionTableViewImage.image = UIImage(named: "TravelUnchecked")
+            }
             attributedString.removeAttribute(NSAttributedString.Key.strikethroughStyle, range: NSRange(location: 0, length: attributedString.length))
             cell.actionViewCellLabel.attributedText = attributedString
         }
         cell.delegate = self
         return cell
     }
+    
+    fileprivate func deleteActionItem(_ indexPath: IndexPath, _ tableView: UITableView) {
+        
+        if totalCarbonSavings > 0 {
+            totalCarbonSavings -= myActions[indexPath.row].carbonReduction ?? 0
+        }
+        myActions.remove(at: indexPath.row)
+        ActionPlanController.shared.deleteAction(index: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .fade)
+        if actionsCompletedCount > 0 {
+            actionsCompletedCount -= 1
+        }
+        updateViews()
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            print("IP: \(indexPath.row)")
+            deleteActionItem(indexPath, tableView)
+        }
+    }
 }
 
 extension MyActionsViewController: ActionTableViewCellDelegate {
-
+    
     func actionChecked(for cell: ActionTableViewCell){
         guard let action = cell.action else { return }
         myActions[cell.tag].completed = true
-        LocalStorageController.shared.saveActions(actions: myActions)
+        ActionPlanController.shared.saveActions(actions: myActions)
         actionsCompletedCount += 1
         let carbonReduction = (action.carbonReduction ?? 0)
         totalCarbonSavings += carbonReduction
@@ -116,7 +142,7 @@ extension MyActionsViewController: ActionTableViewCellDelegate {
     func actionUnchecked(for cell: ActionTableViewCell){
         guard let action = cell.action else { return }
         myActions[cell.tag].completed = false
-        LocalStorageController.shared.saveActions(actions: myActions)
+        ActionPlanController.shared.saveActions(actions: myActions)
         actionsCompletedCount -= 1
         let carbonReduction = (action.carbonReduction ?? 0)
         totalCarbonSavings -= carbonReduction
