e869693036f30f50d386b1135017f018588a67d7
Hanadi <alothman.hanadi@gmail.com>
2019-01-31 16:02:27 -0700

    Fixes Navigation .. i think

---
+++ Conscious/ModelControllers/TransparentNavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  TransparentNavigationController.swift
+//  Conscious
+//
+//  Created by Hanadi AlOthman on 31/01/2019.
+//  Copyright © 2019 Thomas Cowern New. All rights reserved.
+//
+
+import UIKit
+
+class TransparentNavigationController: UINavigationController {
+    // MARK: - Properties
+    override var preferredStatusBarStyle: UIStatusBarStyle { // Sets the status bar style to be light
+        return .lightContent
+    }
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        setupViews()
+    }
+    
+    // MARK: - Setup
+    func setupViews() {
+        navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationBar.shadowImage = UIImage()
+        navigationBar.isTranslucent = true
+        view.backgroundColor = .clear
+    }
+    
+}
